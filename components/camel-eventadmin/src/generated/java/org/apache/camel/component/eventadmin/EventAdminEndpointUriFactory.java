a97249e3b4e4fe38c98a33263f9544781f98dc51
Claus Ibsen <claus.ibsen@gmail.com>
2020-10-12 19:33:21 +0200

    CAMEL-15664: Automatically wrap secret properites with RAW when computing the URI (regen)

--- components/camel-eventadmin/src/generated/java/org/apache/camel/component/eventadmin/EventAdminEndpointUriFactory.java
+++ components/camel-eventadmin/src/generated/java/org/apache/camel/component/eventadmin/EventAdminEndpointUriFactory.java
@@ -2,6 +2,7 @@
 package org.apache.camel.component.eventadmin;
 
 import java.net.URISyntaxException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -17,17 +18,19 @@ public class EventAdminEndpointUriFactory extends org.apache.camel.support.compo
     private static final String BASE = ":topic";
 
     private static final Set<String> PROPERTY_NAMES;
+    private static final Set<String> SECRET_PROPERTY_NAMES;
     static {
-        Set<String> set = new HashSet<>(8);
-        set.add("topic");
-        set.add("send");
-        set.add("bridgeErrorHandler");
-        set.add("exceptionHandler");
-        set.add("exchangePattern");
-        set.add("lazyStartProducer");
-        set.add("basicPropertyBinding");
-        set.add("synchronous");
-        PROPERTY_NAMES = set;
+        Set<String> props = new HashSet<>(8);
+        props.add("basicPropertyBinding");
+        props.add("lazyStartProducer");
+        props.add("bridgeErrorHandler");
+        props.add("synchronous");
+        props.add("exchangePattern");
+        props.add("topic");
+        props.add("send");
+        props.add("exceptionHandler");
+        PROPERTY_NAMES = Collections.unmodifiableSet(props);
+        SECRET_PROPERTY_NAMES = Collections.emptySet();
     }
 
     @Override
@@ -50,6 +53,11 @@ public class EventAdminEndpointUriFactory extends org.apache.camel.support.compo
     @Override
     public Set<String> propertyNames() {
         return PROPERTY_NAMES;
+    }
+
+    @Override
+    public Set<String> secretPropertyNames() {
+        return SECRET_PROPERTY_NAMES;
     }
 
     @Override
