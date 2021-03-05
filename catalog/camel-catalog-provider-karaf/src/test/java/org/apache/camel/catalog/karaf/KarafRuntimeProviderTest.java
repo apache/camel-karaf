cfab84d971ac27c67c7b41d230e7d4ba1784ff16
Claus Ibsen <claus.ibsen@gmail.com>
2021-03-05 18:09:42 +0100

    Fixed karaf catalog test

--- catalog/camel-catalog-provider-karaf/src/test/java/org/apache/camel/catalog/karaf/KarafRuntimeProviderTest.java
+++ catalog/camel-catalog-provider-karaf/src/test/java/org/apache/camel/catalog/karaf/KarafRuntimeProviderTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 import org.apache.camel.catalog.CamelCatalog;
 import org.apache.camel.catalog.DefaultCamelCatalog;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -29,7 +28,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-//@Ignore("TODO: move to camel-karaf")
 public class KarafRuntimeProviderTest {
 
     static CamelCatalog catalog;
@@ -72,7 +70,6 @@ public class KarafRuntimeProviderTest {
 
         // regular components
         assertTrue(names.contains("ahc"));
-        assertTrue(names.contains("aws-s3"));
         assertTrue(names.contains("ftp"));
         assertTrue(names.contains("http"));
         assertTrue(names.contains("jetty"));
