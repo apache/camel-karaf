6bcb62d020e73c3761b8d0b78e6c7dff12da3751
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-04-08 18:29:43 +0900

    CAMEL-16259 - Karaf examples expect org.apache.camel.component.servlet.osgi package

--- components/camel-servlet-osgi/src/main/java/org/apache/camel/component/servlet/osgi/OsgiServletRegisterer.java
+++ components/camel-servlet-osgi/src/main/java/org/apache/camel/component/servlet/osgi/OsgiServletRegisterer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.osgi;
+package org.apache.camel.component.servlet.osgi;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
