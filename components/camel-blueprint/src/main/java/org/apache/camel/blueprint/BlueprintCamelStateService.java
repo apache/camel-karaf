4817150407135bd39395e757ec8c3c476a2d158e
globalbus <jfilipski@gmail.com>
2023-08-25 19:12:45 +0200

    CAMEL-19024 Adding camel-cxf-spring feature, which adds newly introduced spring specific cxf camel bundles (#237)

--- components/camel-blueprint/src/main/java/org/apache/camel/blueprint/BlueprintCamelStateService.java
+++ components/camel-blueprint/src/main/java/org/apache/camel/blueprint/BlueprintCamelStateService.java
@@ -16,8 +16,8 @@
  */
 package org.apache.camel.blueprint;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -63,8 +63,8 @@ public class BlueprintCamelStateService {
      * @param contextId
      * @param state
      */
-    public void setBundleState(Bundle bundle, String contextId, State state) {
-        setBundleState(bundle, contextId, state, null);
+    public void setContextState(String contextId, State state) {
+        setContextState(contextId, state, null);
     }
 
     /**
@@ -74,54 +74,46 @@ public class BlueprintCamelStateService {
      * @param state
      * @param t
      */
-    public void setBundleState(Bundle bundle, String contextId, State state, Throwable t) {
+    public void setContextState(String contextId, State state, Throwable t) {
         if (state == State.Failure) {
-            LOG.warn("Changing Camel state for bundle {} to {}", bundle.getBundleId(), state);
+            LOG.warn("Changing Camel state for bundle {} to {}", bundleContext.getBundle().getBundleId(), state);
         } else if (LOG.isDebugEnabled()) {
-            LOG.debug("Changing Camel state for bundle {} to {}", bundle.getBundleId(), state);
+            LOG.debug("Changing Camel state for bundle {} to {}", bundleContext.getBundle().getBundleId(), state);
         }
 
-        String key = String.format("%d:%s", bundle.getBundleId(), contextId);
         if (state != null) {
-            states.put(key, state);
+            states.put(contextId, state);
         } else {
-            states.remove(key);
+            states.remove(contextId);
         }
         if (t != null) {
-            exceptions.put(key, t);
+            exceptions.put(contextId, t);
         } else {
-            exceptions.remove(key);
+            exceptions.remove(contextId);
         }
     }
 
     /**
-     * Get states for all context registered for given {@link Bundle}
+     * Get states for all context registered for {@link Bundle}
      * @param bundle
      * @return
      */
-    public List<State> getStates(Bundle bundle) {
-        List<State> result = new LinkedList<>();
-        for (Map.Entry<String, State> e : states.entrySet()) {
-            if (e.getKey().startsWith(bundle.getBundleId() + ":")) {
-                result.add(e.getValue());
-            }
+    public Collection<State> getStates(Bundle bundle) {
+        //return only states if provided bundle equals this bundle
+        if (bundle == bundleContext.getBundle()) {
+            return Collections.unmodifiableCollection(states.values());
+        } else {
+            return List.of();
         }
-        return result;
     }
 
     /**
-     * Get exceptions for all camel contexts for given bundle
-     * @param bundle
+     * Get exceptions for all camel contexts for this bundle
+     *
      * @return
      */
-    public Map<String, Throwable> getExceptions(Bundle bundle) {
-        Map<String, Throwable> result = new LinkedHashMap<>();
-        for (Map.Entry<String, Throwable> e : exceptions.entrySet()) {
-            if (e.getKey().startsWith(bundle.getBundleId() + ":")) {
-                result.put(e.getKey().substring(e.getKey().indexOf(":") + 1), e.getValue());
-            }
-        }
-        return result;
+    public Map<String, Throwable> getExceptions() {
+        return Collections.unmodifiableMap(exceptions);
     }
 
     /**
