d88c1f0e91674a98faba86c679bddff9462399a0
Francois de Parscau <francois.deparscau@qlik.com>
2024-05-30 13:01:51 +0200

    Add camel-bean test

---
+++ tests/features/camel-mail/src/main/java/org/apache/karaf/camel/test/CamelMailRouteSupplier.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.karaf.camel.test;
+
+import java.util.function.Function;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.karaf.camel.itests.AbstractCamelSingleFeatureResultMockBasedRouteSupplier;
+import org.apache.karaf.camel.itests.CamelRouteSupplier;
+import org.osgi.service.component.annotations.Component;
+
+
+@Component(
+        name = "karaf-camel-mail-test",
+        immediate = true,
+        service = CamelRouteSupplier.class
+)
+public class CamelMailRouteSupplier extends AbstractCamelSingleFeatureResultMockBasedRouteSupplier {
+
+
+    @Override
+    protected Function<RouteBuilder, RouteDefinition> consumerRoute() {
+        return builder ->
+                builder.from(
+                        "pop3://camel@localhost:%s?password=foo&initialDelay=100&delay=500"
+                                .formatted(System.getProperty("pop3.port"))).log("received message ${body} from:${header.from} to:${header.to} subj: ${header.subject}");
+    }
+
+    @Override
+    protected void configureProducer(RouteBuilder builder, RouteDefinition producerRoute) {
+        producerRoute.log("calling mail endpoint")
+                .setBody(builder.constant("OK"))
+                .setHeader("Subject", builder.constant("Test"))
+                .setHeader("From", builder.constant("origin@localhost"))
+                .setHeader("To", builder.constant("camel@localhost"))
+                .to("smtp://camel@localhost:%s?password=foo".formatted(System.getProperty("smtp.port")));
+    }
+}
+
---
+++ tests/features/camel-mail/src/test/java/org/apache/karaf/camel/itest/CamelMailITest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.camel.itest;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.karaf.camel.itests.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+
+@CamelKarafTestHint(externalResourceProvider = CamelMailITest.ExternalResourceProviders.class)
+@RunWith(PaxExamWithExternalResource.class)
+@ExamReactorStrategy(PerClass.class)
+public class CamelMailITest extends AbstractCamelSingleFeatureResultMockBasedRouteITest {
+
+    @Override
+    public void configureMock(MockEndpoint mock) {
+        mock.expectedBodiesReceived("OK\r\n");
+    }
+
+    @Test
+    public void testResultMock() throws Exception {
+        assertMockEndpointsSatisfied();
+    }
+
+    public static class MailContainer extends FixedHostPortGenericContainer<MailContainer> {
+        public MailContainer(String dockerImage) {
+            super(dockerImage);
+        }
+    }
+
+    public static final class ExternalResourceProviders {
+        public static GenericContainerResource<MailContainer> createGreenMailContainer() {
+            int smtpport = Utils.getNextAvailablePort();
+            int pop3port = Utils.getNextAvailablePort(port -> port != smtpport);
+            final MailContainer greenMailContainer =
+                    new MailContainer("greenmail/standalone:2.0.1").withFixedExposedPort(smtpport, 3025)
+                            .withFixedExposedPort(pop3port, 3110)
+                            .withEnv("GREENMAIL_OPTS", "-Dgreenmail.users=camel:foo@localhost -Dgreenmail.setup.test.all -Dgreenmail.hostname=0.0.0.0 -Dgreenmail.auth.disabled");
+
+            return new GenericContainerResource<>(greenMailContainer, resource -> {
+                resource.setProperty("smtp.port", "%s".formatted(smtpport));
+                resource.setProperty("pop3.port", "%s".formatted(pop3port));
+            });
+        }
+    }
+}
