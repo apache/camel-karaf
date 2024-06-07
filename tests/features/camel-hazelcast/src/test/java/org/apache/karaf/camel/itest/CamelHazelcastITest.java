58423daa59f2dbdd12b0c71bf59f27a3016ca753
Francois de Parscau <francois.deparscau@qlik.com>
2024-06-07 17:15:05 +0200

    Add hazelcast integration test

---
+++ tests/features/camel-hazelcast/src/test/java/org/apache/karaf/camel/itest/CamelHazelcastITest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.camel.itest;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.karaf.camel.itests.AbstractCamelSingleFeatureResultMockBasedRouteITest;
+import org.apache.karaf.camel.itests.AvailablePortProvider;
+import org.apache.karaf.camel.itests.CamelKarafTestHint;
+import org.apache.karaf.camel.itests.PaxExamWithExternalResource;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+@CamelKarafTestHint(externalResourceProvider = CamelHazelcastITest.ExternalResourceProviders.class)
+@RunWith(PaxExamWithExternalResource.class)
+@ExamReactorStrategy(PerClass.class)
+public class CamelHazelcastITest extends AbstractCamelSingleFeatureResultMockBasedRouteITest {
+
+    private static final int HZ_PORT = 5701;
+
+    @Override
+    public void configureMock(MockEndpoint mock) {
+        //Map + replicated Map + 2 for list + Queue + Set + Topic
+        mock.expectedBodiesReceived( Collections.nCopies(7, "OK"));
+    }
+
+    @Test
+    public void testResultMock() throws Exception {
+        assertMockEndpointsSatisfied();
+    }
+
+    @After
+    public void stopCamelContext() {
+        //to trigger graceful shutdown of hazelcast components before uninstalling bundles
+        getContext().stop();
+    }
+
+    public static final class ExternalResourceProviders {
+        public static AvailablePortProvider createAvailablePortProvider() {
+            return new AvailablePortProvider(List.of("hz.port"));
+        }
+    }
+}
