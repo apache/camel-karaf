0bf3d0bb4f792ceb4f04d926d49f5a8bc54aa1a2
Francois de Parscau <francois.deparscau@qlik.com>
2024-07-25 10:10:05 +0200

    Add camel-consul integration test

---
+++ tests/features/camel-consul/src/test/java/org/apache/karaf/camel/itest/CamelConsulITest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.camel.itest;
+
+import java.util.function.Consumer;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.karaf.camel.itests.AbstractCamelSingleFeatureResultMockBasedRouteITest;
+import org.apache.karaf.camel.itests.CamelKarafTestHint;
+import org.apache.karaf.camel.itests.GenericContainerResource;
+import org.apache.karaf.camel.itests.PaxExamWithExternalResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import com.orbitz.consul.Consul;
+
+@CamelKarafTestHint(externalResourceProvider = CamelConsulITest.ExternalResourceProviders.class)
+@RunWith(PaxExamWithExternalResource.class)
+@ExamReactorStrategy(PerClass.class)
+public class CamelConsulITest extends AbstractCamelSingleFeatureResultMockBasedRouteITest {
+
+    @Override
+    public void configureMock(MockEndpoint mock) {
+        mock.expectedBodiesReceived("OK");
+    }
+
+    @Test
+    public void testResultMock() throws Exception {
+        assertMockEndpointsSatisfied();
+    }
+
+    public static final class ExternalResourceProviders {
+
+        public static GenericContainerResource createConsulContainer() {
+            final GenericContainer<?> consulContainer =
+                    new GenericContainer<>("hashicorp/consul:1.19")
+                            .withExposedPorts(Consul.DEFAULT_HTTP_PORT)
+                            .waitingFor(Wait.forLogMessage(".*Synced node info.*", 1))
+                            .withCommand("agent", "-dev", "-server", "-bootstrap", "-client", "0.0.0.0", "-log-level", "trace");
+
+            return new GenericContainerResource(consulContainer, (Consumer<GenericContainerResource>) resource -> {
+                resource.setProperty("consul.port", Integer.toString(consulContainer.getMappedPort(Consul.DEFAULT_HTTP_PORT)));
+            });
+        }
+    }
+}
