3dd12a43c2b7ae34a043224504d34399044919a4
Nicolas Filotto <essobedo@users.noreply.github.com>
2024-09-16 17:53:07 +0200

    Ref #494: Upgrade to Camel 4.8 (#496)

--- tests/features/camel-ehcache/src/test/java/org/apache/karaf/camel/itest/CamelEhcacheITest.java
+++ tests/features/camel-ehcache/src/test/java/org/apache/karaf/camel/itest/CamelEhcacheITest.java
@@ -15,13 +15,13 @@ package org.apache.karaf.camel.itest;
 
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.karaf.camel.itests.AbstractCamelSingleFeatureResultMockBasedRouteITest;
-import org.apache.karaf.camel.itests.PaxExamWithExternalResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
-@RunWith(PaxExamWithExternalResource.class)
+@RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class CamelEhcacheITest extends AbstractCamelSingleFeatureResultMockBasedRouteITest {
 
