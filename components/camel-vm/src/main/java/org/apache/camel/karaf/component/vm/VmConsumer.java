442160c72d3c84e9bb197b519a332f96abbbef46
Nicolas Filotto <essobedo@users.noreply.github.com>
2024-10-30 10:33:28 +0100

    Ref #542: Restore camel-vm old behavior (#543)

--- components/camel-vm/src/main/java/org/apache/camel/karaf/component/vm/VmConsumer.java
+++ components/camel-vm/src/main/java/org/apache/camel/karaf/component/vm/VmConsumer.java
@@ -16,14 +16,13 @@
  */
 package org.apache.camel.karaf.component.vm;
 
-import org.apache.camel.*;
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelContextAware;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangeExtension;
+import org.apache.camel.Processor;
 import org.apache.camel.component.seda.SedaConsumer;
-import org.apache.camel.support.DefaultExchange;
-
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import org.apache.camel.support.ExchangeHelper;
 
 public class VmConsumer extends SedaConsumer implements CamelContextAware {
 
@@ -43,4 +42,23 @@ public class VmConsumer extends SedaConsumer implements CamelContextAware {
         this.camelContext = camelContext;
     }
 
+    @Override
+    protected Exchange prepareExchange(Exchange exchange) {
+        Exchange result = ExchangeHelper.copyExchangeWithProperties(exchange, camelContext);
+        ExchangeExtension exchangeExtension = result.getExchangeExtension();
+        exchangeExtension.setFromEndpoint(getEndpoint());
+        exchangeExtension.setFromRouteId(getRouteId());
+        return result;
+    }
+
+    @Override
+    protected void onProcessingDone(Exchange original, Exchange prepared) {
+        // copy result back
+        ExchangeHelper.copyResults(original, prepared);
+        // log exception if an exception occurred and was not handled
+        if (original.getException() != null) {
+            getExceptionHandler().handleException("Error processing exchange", original,
+                    original.getException());
+        }
+    }
 }
