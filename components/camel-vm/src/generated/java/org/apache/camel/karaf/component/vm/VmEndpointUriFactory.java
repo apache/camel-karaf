c1f0831727330376c0b75355f99453add103ff71
JB Onofré <jbonofre@apache.org>
2026-03-18 14:21:04 +0100

    Fix compilation failure: update multiValuePrefixes() return type to Map<String, String>
    Camel 4.18.0 changed EndpointUriFactory.multiValuePrefixes() from Set<String>
    to Map<String, String>. Updated the generated DirectVm and Vm endpoint URI
    factories accordingly.

--- components/camel-vm/src/generated/java/org/apache/camel/karaf/component/vm/VmEndpointUriFactory.java
+++ components/camel-vm/src/generated/java/org/apache/camel/karaf/component/vm/VmEndpointUriFactory.java
@@ -21,7 +21,7 @@ public class VmEndpointUriFactory extends org.apache.camel.support.component.End
 
     private static final Set<String> PROPERTY_NAMES;
     private static final Set<String> SECRET_PROPERTY_NAMES;
-    private static final Set<String> MULTI_VALUE_PREFIXES;
+    private static final Map<String, String> MULTI_VALUE_PREFIXES;
     static {
         Set<String> props = new HashSet<>(19);
         props.add("blockWhenFull");
@@ -45,7 +45,7 @@ public class VmEndpointUriFactory extends org.apache.camel.support.component.End
         props.add("waitForTaskToComplete");
         PROPERTY_NAMES = Collections.unmodifiableSet(props);
         SECRET_PROPERTY_NAMES = Collections.emptySet();
-        MULTI_VALUE_PREFIXES = Collections.emptySet();
+        MULTI_VALUE_PREFIXES = Collections.emptyMap();
     }
 
     @Override
@@ -76,7 +76,7 @@ public class VmEndpointUriFactory extends org.apache.camel.support.component.End
     }
 
     @Override
-    public Set<String> multiValuePrefixes() {
+    public Map<String, String> multiValuePrefixes() {
         return MULTI_VALUE_PREFIXES;
     }
 
